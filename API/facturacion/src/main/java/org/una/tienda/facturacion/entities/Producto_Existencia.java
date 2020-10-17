aa8302bd067158b7dc0fef21bef9305e286baf1f
RacheVC <rachecordero12@gmail.com>
2020-10-16 19:12:36 -0600

    Entities y demas de producto Existencia

---
+++ API/facturacion/src/main/java/org/una/tienda/facturacion/entities/Producto_Existencia.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.tienda.facturacion.entities;
+
+/**
+ *
+ * @author rache
+ */
+public class Producto_Existencia {
+    
+}
