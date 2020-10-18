9a580176ab33930ee38294ff299bcb9783878ad8
andres7xd <58347580+andres7xd@users.noreply.github.com>
2020-10-18 17:33:11 -0600

    prueba
    pruebas

--- API/facturacion/src/test/java/org/una/tienda/facturacion/services/FacturaDetallesServiceImplementationTest.java
+++ API/facturacion/src/test/java/org/una/tienda/facturacion/services/FacturaDetallesServiceImplementationTest.java
@@ -53,6 +53,7 @@ public class FacturaDetallesServiceImplementationTest {
     ClienteDTO clientePrueba;
     FacturaDTO facturaPrueba;
     Factura_DetallesDTO facturaDetallePruebaConExtraDescuento;
+    Factura_DetallesDTO facturaDetallePrueba;
 
     @BeforeEach
     public void setup() {
@@ -63,62 +64,9 @@ public class FacturaDetallesServiceImplementationTest {
             }
         };
     }
-
-    @Test
-    public void sePuedeCrearUnaFacturaDetallesCorrectamente() {
-        
-        initDataForSeEvitaFacturarUnProductoConDescuentoMayorAlPermitido();
-        try {
-             factura_DetallesEjemplo = facturaDetallesService.create(facturaDetallePruebaConExtraDescuento);
-        } catch (Exception e) {
-            fail("Producto con extradescuento");
-        }
-       
-
-        Optional<Factura_DetallesDTO> facturaDetallesEncontrado = facturaDetallesService.findById(factura_DetallesEjemplo.getId());
-
-        if (facturaDetallesEncontrado.isPresent()) {
-            Factura_DetallesDTO facturaDetalles = facturaDetallesEncontrado.get();
-            assertEquals(factura_DetallesEjemplo.getId(), facturaDetalles.getId());
-
-        } else {
-            fail("No se encontro la información en la BD");
-        }
-    }
-
-    public void sePuedeModificarUnaFacturaDetallesCorrectamente() {
-
-        Optional<Factura_DetallesDTO> facturDetails = facturaDetallesService.update(factura_DetallesEjemplo, factura_DetallesEjemplo.getId());
-
-        Optional<Factura_DetallesDTO> factura_DetallesEncontrado = facturaDetallesService.findById(factura_DetallesEjemplo.getId());
-
-        if (factura_DetallesEncontrado.isPresent()) {
-            Factura_DetallesDTO factura_DetallesDTO = factura_DetallesEncontrado.get();
-            assertEquals(factura_DetallesEjemplo.getId(), factura_DetallesDTO.getId());
-
-        } else {
-            fail("No se encontro la información en la BD");
-        }
-    }
-
-//    @Test
-//    public void sePuedeEliminarUnaFacturaDetallesCorrectamente() throws ProductoConDescuentoMayorAlPermitidoException {
-//
-//        factura_DetallesEjemplo = facturaDetallesService.create(factura_DetallesEjemplo);
-//
-//        facturaDetallesService.delete(factura_DetallesEjemplo.getId());
-//
-//        Optional<Factura_DetallesDTO> facturaEncontrado = facturaDetallesService.findById(factura_DetallesEjemplo.getId());
-//
-//        if (facturaEncontrado != null) {
-//            fail("El producto no fue eliminado");
-//        } else {
-//            Assertions.assertTrue(true);
-//            factura_DetallesEjemplo = null;
-//        }
-//    }
-
-   private void initDataForSeEvitaFacturarUnProductoConDescuentoMayorAlPermitido() {
+    
+    
+     private void initDataForSeEvitaFacturarUnProductoConDescuentoMayorAlPermitido() {
         productoPrueba = new ProductoDTO() {
             {
                 setDescripcion("Producto De Ejemplo");
@@ -178,10 +126,131 @@ public class FacturaDetallesServiceImplementationTest {
                 setDescuento_final(productoPrecioPrueba.getDescuento_maximo()+1);
             }
         };
+        
+      
 
  
 
     }
+   
+   private void initDataForSePuedeCrearUnaFacturaDetallesCorrectamente() throws ProductoConDescuentoMayorAlPermitidoException {
+       
+       productoPrueba = new ProductoDTO() {
+            {
+                
+                setDescripcion("hola");
+                setImpuesto(0.25);
+            }
+        };
+        productoPrueba = productoService.create(productoPrueba);
+        
+         productoExistenciaPrueba = new Producto_ExistenciaDTO() {
+            {
+                setUt_productos(productoPrueba);
+                setCantidad(1);
+            }
+        };
+        productoExistenciaPrueba = productoExistenciaService.create(productoExistenciaPrueba);
+        
+         productoPrecioPrueba = new Producto_PrecioDTO() {
+            {
+                setUt_productos(productoPrueba);
+                setPrecio_colones(1000);
+                setDescuento_maximo(10);
+                setDescuento_promocional(2);
+            }
+        };
+        productoPrecioPrueba = productoPrecioService.create(productoPrecioPrueba);
+        
+     
+
+        clientePrueba = new ClienteDTO() {
+            {
+                setNombre("ClienteDePrueba");
+            }
+        };
+        clientePrueba = clienteService.create(clientePrueba);
+
+        facturaPrueba = new FacturaDTO() {
+            {
+                setCaja(991);
+                setUt_clientes(clientePrueba);
+            }
+        };
+        facturaPrueba = facturaService.create(facturaPrueba);
+
+        
+        
+         
+        
+        facturaDetallePrueba = new Factura_DetallesDTO() {
+            {
+                setCantidad(1);
+                setUt_productos(productoPrueba);
+                setUt_facturas(facturaPrueba);
+                setDescuento_final(productoPrecioPrueba.getDescuento_maximo());
+            }
+        };
+        facturaDetallePrueba = facturaDetallesService.create(facturaDetallePrueba);
+    }
+
+    @Test
+    public void sePuedeCrearUnaFacturaDetallesCorrectamente() throws ProductoConDescuentoMayorAlPermitidoException {
+        
+        
+        initDataForSePuedeCrearUnaFacturaDetallesCorrectamente();
+        
+        try {
+             factura_DetallesEjemplo = facturaDetallesService.create(facturaDetallePrueba);
+        } catch (ProductoConDescuentoMayorAlPermitidoException e) {
+            fail(e);
+        }
+       
+
+        Optional<Factura_DetallesDTO> facturaDetallesEncontrado = facturaDetallesService.findById(factura_DetallesEjemplo.getId());
+
+        if (facturaDetallesEncontrado.isPresent()) {
+            Factura_DetallesDTO facturaDetalles = facturaDetallesEncontrado.get();
+            assertEquals(factura_DetallesEjemplo.getId(), facturaDetalles.getId());
+
+        } else {
+            fail("No se encontro la información en la BD");
+        }
+    }
+
+    public void sePuedeModificarUnaFacturaDetallesCorrectamente() {
+
+        Optional<Factura_DetallesDTO> facturDetails = facturaDetallesService.update(factura_DetallesEjemplo, factura_DetallesEjemplo.getId());
+
+        Optional<Factura_DetallesDTO> factura_DetallesEncontrado = facturaDetallesService.findById(factura_DetallesEjemplo.getId());
+
+        if (factura_DetallesEncontrado.isPresent()) {
+            Factura_DetallesDTO factura_DetallesDTO = factura_DetallesEncontrado.get();
+            assertEquals(factura_DetallesEjemplo.getId(), factura_DetallesDTO.getId());
+
+        } else {
+            fail("No se encontro la información en la BD");
+        }
+    }
+
+//    @Test
+//    public void sePuedeEliminarUnaFacturaDetallesCorrectamente() throws ProductoConDescuentoMayorAlPermitidoException {
+//
+//        factura_DetallesEjemplo = facturaDetallesService.create(factura_DetallesEjemplo);
+//
+//        facturaDetallesService.delete(factura_DetallesEjemplo.getId());
+//
+//        Optional<Factura_DetallesDTO> facturaEncontrado = facturaDetallesService.findById(factura_DetallesEjemplo.getId());
+//
+//        if (facturaEncontrado != null) {
+//            fail("El producto no fue eliminado");
+//        } else {
+//            Assertions.assertTrue(true);
+//            factura_DetallesEjemplo = null;
+//        }
+//    }
+
+  
 
         @Test
         public void seEvitaFacturarUnProductoConDescuentoMayorAlPermitido() { 
@@ -206,15 +275,14 @@ public class FacturaDetallesServiceImplementationTest {
 //    
 //    
 //    
-//}
+
 //        @AfterEach
-//        public void tearDown
-//        
-//            () {
+//        public void tearDown() {
 //        if (factura_DetallesEjemplo != null) {
 //                facturaDetallesService.delete(factura_DetallesEjemplo.getId());
 //                factura_DetallesEjemplo = null;
 //            }
 //
 //        }
-    }
+
+}
